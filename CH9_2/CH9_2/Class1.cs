e385fa14a6facd4190093cf91f38bddbc5f1193e
Mamamoo4 <elmer0219.hsu@gmail.com>
2025-05-06 10:16:24 +0800

    Add files via upload

---
+++ CH9_2/CH9_2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH9_2
+{
+    internal class Box
+    {
+        public double length { get; set; }
+        public double width { get; set; }
+        public double height { get; set; }
+
+        public Box(double length, double width, double height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+       public double Volume()
+        {           
+            return length * width * height;
+        }
+
+        public double Area()
+        {
+            return 2 * (length * width + length * height + width * height);
+        }
+    }
+}
---
+++ CH9_2/CH9_2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CH9_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double length = double.Parse(textBox1.Text);
+                double width = double.Parse(textBox2.Text);
+                double height = double.Parse(textBox3.Text);
+                Box calculate = new Box(length, width, height);
+                label4.Text = $" 體積 = {calculate.Volume():F2} 立方公分 ";
+            }
+            catch
+            {
+                label4.Text = $"輸入錯誤 , 請重新輸入";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double length = double.Parse(textBox1.Text);
+                double width = double.Parse(textBox2.Text);
+                double height = double.Parse(textBox3.Text);
+                Box calculate = new Box(length, width, height);
+                label4.Text = $"表面積和 = {calculate.Area():F2} 平方公分";
+            }
+            catch
+            {
+                label4.Text = $"輸入錯誤 , 請重新輸入";
+            }
+        }
+    }
+}
